01e47d755605dc608d93e30fd39604b95065a4de
Faiji Akbar Liam <faijiakbarliam2016@gmail.com>
2026-05-14 02:36:14 +0600

    first commit

---
+++ src/app/not-found.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className='text-cenetr font-bold items-center h-screen flex justify-center text-5xl'>
+      Not Found
+    </div>
+  )
+}
+
+export default NotFound
