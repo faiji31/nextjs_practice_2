263dd0446e539c6cbe32d1e434bcf919690554f7
Faiji Akbar Liam <faijiakbarliam2016@gmail.com>
2026-05-16 04:25:37 +0600

    7th commit

--- src/app/(withcommonLayout)/services/page.jsx
+++ src/app/(withcommonLayout)/services/page.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 
-const SevicePage = () => {
+
+const getAllServices = async()=>{
+  const res = await fetch("https://raw.githubusercontent.com/ferdouszihad/Hero-Kidz-assets/refs/heads/main/toys.json")
+  const data = await res.json()
+  return data
+}
+
+const SevicePage = async() => {
+  const services = await getAllServices()
+  console.log(services)
   return (
     <div>
       <h2>Service Page</h2>
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,16 +1,27 @@
 import Link from 'next/link'
 import React from 'react'
 
+import Container from './Container'
+
 const Navbar = () => {
   return (
-    <div className='h-[150px] bg-blue-800 text-black flex items-center justify-center font-bold '>
+
+
+    <div className='h-[150px] bg-blue-800 text-black flex items-center justify-between font-bold p-6 '>
       <ul className='flex gap-5'>
         <Link href={"/"}>Home</Link>
         <Link href={"/services"}>Services</Link>
         <Link href={"about"}>About Us</Link>
         <Link href={"/reviews"}>Reviews</Link>
       </ul>
+
+      <Link href={"/login"}>
+      <button>  Login</button>
+   
+      </Link>
     </div>
+
+    
   )
 }
 
