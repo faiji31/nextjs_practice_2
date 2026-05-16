c1d966d4c68c66158cdeef76515b26e78ae6b892
Faiji Akbar Liam <faijiakbarliam2016@gmail.com>
2026-05-16 06:01:29 +0600

    12th commit

--- src/app/(withcommonLayout)/services/page.jsx
+++ src/app/(withcommonLayout)/services/page.jsx
@@ -1,40 +1,34 @@
-import React from "react";
-import ServiceCard from "../../../components/ServiceCard";
+import React from 'react'
+import ServiceCard from "../../../components/ServiceCard"
 
-const getAllServices = async () => {
-  const res = await fetch(
-    "https://raw.githubusercontent.com/ferdouszihad/Hero-Kidz-assets/refs/heads/main/toys.json",
-    {
-      // Optional: cache data in Next.js
-      next: { revalidate: 60 },
-    }
-  );
+const getAllServices = async()=>{
+  const res = await fetch("https://raw.githubusercontent.com/ferdouszihad/Hero-Kidz-assets/refs/heads/main/toys.json")
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch services");
-  }
+  await new Promise((resolve)=>setTimeout(resolve, 3000))
+  const data = await res.json()
+  return data
+}
 
-  const data = await res.json();
-  return data;
-};
-
-const SevicePage = async () => {
-  const services = await getAllServices();
-
+const ServicePage = async() => {
+  const services = await getAllServices()
+  console.log(services)
   return (
-    <div className="max-w-7xl mx-auto px-4 py-10">
-      <h2 className="text-3xl font-bold mb-8">Service Page</h2>
+    <div >
+      <h2>Service Page</h2>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-        {services.map((service, index) => (
+      <div className='grid grid-cols-4 gap-5'>
+     
+
+      {
+        services.map((service, index) => (
           <div key={service.title}>
-            {/* Pass both service and index */}
             <ServiceCard service={service} index={index} />
           </div>
-        ))}
-      </div>
+        ))
+      }
+       </div>
     </div>
-  );
-};
+  )
+}
 
-export default SevicePage;
+export default ServicePage
